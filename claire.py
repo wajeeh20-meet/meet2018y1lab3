60e9d5fd226d156e5d354ae1efb18559ae820dfc
wajeeh20-meet <wajeeh20@meet.mit.edu>
2018-07-31 09:34:59 -0400

    Stopping here

---
+++ claire.py
@@ -0,0 +1,2 @@
+name="Hi there, my name is Claire. Nice to meet you!"
+print(name[20:27])
--- greeting.py
+++ greeting.py
@@ -1,6 +1,19 @@
-name= input("what's your name?")
+name= input("what's your name? ")
 length= len(name)
+print ("your name is "+ str(length)+' letters long!')
 if len(name)>10:
-    print ("your name is "+ str(length)+' letters long!')
+    print('your name is too long!')
 else:
-    print("hello there, " + name)
+    print("hello there, "+name.capitalize())
+firstle=name[0]
+lastle=name[-1]
+print('the first letter of your name is: '+firstle.capitalize())
+print('the last letter of your name is: '+lastle.capitalize())
+mood=input("so how do you feel today? ")
+mood=mood.lower()
+if mood=='good':
+    print("Oh, I'm glad to hear that:)")
+if mood=='bad':
+    print("I hope you feel better!")
+
+
